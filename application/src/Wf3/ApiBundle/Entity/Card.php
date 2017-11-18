bdeb3ffa9aa0c59e12dbe1a3d00168b8b89db0f9
Loic <lowkick974@gmail.com>
2017-11-18 21:34:04 +0100

    new db for api. Creation Entities

---
+++ application/src/Wf3/ApiBundle/Entity/Card.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace Wf3\ApiBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Card
+ *
+ * @ORM\Table(name="card")
+ * @ORM\Entity(repositoryClass="Wf3\ApiBundle\Repository\CardRepository")
+ */
+class Card
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="numero", type="string", length=10, nullable=true)
+     */
+    private $numero;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="code", type="string", length=6)
+     */
+    private $code;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created_at", type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @var CenterRequest
+     * @ORM\ManyToOne(targetEntity="ApiBundle\Entity\CenterRequest")
+     */
+    private $centerRequest;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set numero
+     *
+     * @param string $numero
+     *
+     * @return Card
+     */
+    public function setNumero($numero)
+    {
+        $this->numero = $numero;
+
+        return $this;
+    }
+
+    /**
+     * Get numero
+     *
+     * @return string
+     */
+    public function getNumero()
+    {
+        return $this->numero;
+    }
+
+    /**
+     * Set code
+     *
+     * @param string $code
+     *
+     * @return Card
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    /**
+     * Get code
+     *
+     * @return string
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     *
+     * @return Card
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set centerRequest
+     *
+     * @param \ApiBundle\Entity\CenterRequest $centerRequest
+     *
+     * @return Card
+     */
+    public function setCenterRequest(\ApiBundle\Entity\CenterRequest $centerRequest = null)
+    {
+        $this->centerRequest = $centerRequest;
+
+        return $this;
+    }
+
+    /**
+     * Get centerRequest
+     *
+     * @return \ApiBundle\Entity\CenterRequest
+     */
+    public function getCenterRequest()
+    {
+        return $this->centerRequest;
+    }
+}
---
+++ application/src/Wf3/ApiBundle/Entity/CenterRequest.php
@@ -0,0 +1,188 @@
+<?php
+
+namespace Wf3\ApiBundle\Entity;
+
+use AppBundle\Entity\Center;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * CenterRequest
+ *
+ * @ORM\Table(name="center_request")
+ * @ORM\Entity(repositoryClass="Wf3\ApiBundle\Repository\CenterRequestRepository")
+ */
+class CenterRequest
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="quantity", type="integer")
+     */
+    private $quantity;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created_at", type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="answered_at", type="datetime", nullable=true)
+     */
+    private $answeredAt;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="status", type="string", length=20, nullable=true)
+     */
+    private $status;
+
+    /**
+     * @var Center
+     * @ORM\ManyToOne(targetEntity="ApiBundle\Entity\Center")
+     */
+    private $center;
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set quantity
+     *
+     * @param integer $quantity
+     *
+     * @return CenterRequest
+     */
+    public function setQuantity($quantity)
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    /**
+     * Get quantity
+     *
+     * @return int
+     */
+    public function getQuantity()
+    {
+        return $this->quantity;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     *
+     * @return CenterRequest
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set answeredAt
+     *
+     * @param \DateTime $answeredAt
+     *
+     * @return CenterRequest
+     */
+    public function setAnsweredAt($answeredAt)
+    {
+        $this->answeredAt = $answeredAt;
+
+        return $this;
+    }
+
+    /**
+     * Get answeredAt
+     *
+     * @return \DateTime
+     */
+    public function getAnsweredAt()
+    {
+        return $this->answeredAt;
+    }
+
+    /**
+     * Set status
+     *
+     * @param string $status
+     *
+     * @return CenterRequest
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * Get status
+     *
+     * @return string
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * Set center
+     *
+     * @param \ApiBundle\Entity\Center $center
+     *
+     * @return CenterRequest
+     */
+    public function setCenter(\ApiBundle\Entity\Center $center = null)
+    {
+        $this->center = $center;
+
+        return $this;
+    }
+
+    /**
+     * Get center
+     *
+     * @return \ApiBundle\Entity\Center
+     */
+    public function getCenter()
+    {
+        return $this->center;
+    }
+}
