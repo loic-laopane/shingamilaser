b5e84e97357700cf1794dd6b9a6512cac8617f71
Loic <lowkick974@gmail.com>
2017-11-18 21:12:45 +0100

    New Bundle Wf3/ApiBundle

---
+++ application/src/Wf3/ApiBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Wf3\ApiBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('ApiBundle:Default:index.html.twig');
+    }
+}
