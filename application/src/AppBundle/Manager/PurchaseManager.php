1a3b0640ad787f3c88ec370df95cbf75267c944f
Loic Lao-Pane <lowkick974@gmail.com>
2017-11-21 14:03:44 +0100

    workflow work on purchase entity

--- application/src/AppBundle/Manager/PurchaseManager.php
+++ application/src/AppBundle/Manager/PurchaseManager.php
@@ -19,6 +19,8 @@ use Psr\Http\Message\ResponseInterface;
 use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Workflow\Transition;
+use Symfony\Component\Workflow\Workflow;
 
 class PurchaseManager
 {
@@ -44,8 +46,13 @@ class PurchaseManager
     private $client;
 
     private $base_uri = 'http://localhost/web/app_dev.php/api/';
+    /**
+     * @var Workflow
+     */
+    private $workflow;
 
-    public function __construct(ObjectManager $objectManager,
+    public function __construct(Workflow $workflow,
+                                ObjectManager $objectManager,
                                 SessionInterface $session,
                                 EventDispatcherInterface $dispatcher,
                                 Serializer $serializer
@@ -56,6 +63,7 @@ class PurchaseManager
         $this->session = $session;
         $this->dispatcher = $dispatcher;
         $this->serializer = $serializer;
+        $this->workflow = $workflow;
     }
 
     public function save(Purchase $purchase)
@@ -75,19 +83,24 @@ class PurchaseManager
         return $this;
     }
 
+    /**
+     * @param $numero
+     * @return Purchase|null|object
+     */
     public function search($numero)
     {
        return $this->repository->findOneBy(['numero' => $numero]);
     }
 
+
     /**
-     * @param
+     * @param Purchase $purchase
+     * @param ResponseInterface $response
+     * @return $this
      */
     public function thread(Purchase $purchase, ResponseInterface $response)
     {
-        $headers = $response->getHeaders();
-        $content = $response->getBody()->getContents();;
-        dump($response);
+        $content = $response->getBody()->getContents();
         $data = $this->serializer->deserialize($content, 'array', 'json');
         $return = array();
         $return['status'] = $data['status_code'];
@@ -97,26 +110,44 @@ class PurchaseManager
             $this->setErrorStatus($purchase);
             $this->session->getFlashBag()->add('danger', $data['message']);
         }
+
         if(isset($data['request_id'])) {
+
             $this->setRequestedStatus($purchase, $data['request_id']);
             $this->session->getFlashBag()->add('success', $data['message']);
         }
+
         return $this;
     }
 
+    /**
+     * @param Purchase $purchase
+     * @param $reference
+     */
     private function setRequestedStatus(Purchase $purchase, $reference)
     {
-        $purchase->setStatus('requested');
+        if (!$this->workflow->can($purchase, 'request')) {
+            throw new Exception('Purchase cannot be requested');
+        }
+        $this->workflow->apply($purchase, 'request');
         $purchase->setReference($reference);
         $purchase->setUpdatedAt(new \DateTime());
         $this->save($purchase);
     }
+
+    /**
+     * @param Purchase $purchase
+     * @return $this
+     */
     private function setErrorStatus(Purchase $purchase)
     {
-        $purchase->setStatus('error');
+        if(!$this->workflow->can($purchase, 'error'))
+        {
+            throw new Exception('Purchase cannot be as error status');
+        }
+        $this->workflow->apply($purchase, 'error');
         $purchase->setUpdatedAt(new \DateTime());
         $this->save($purchase);
-
         return $this;
 
     }
@@ -130,15 +161,30 @@ class PurchaseManager
         $quantity = $purchase->getQuantity();
         $user = $purchase->getRequester();
 
+
         if(null === $user) {
+            $this->setErrorStatus($purchase);
             throw new Exception('No requester found on this purchase');
         }
         if(null === $user->getCenter()) {
+            $this->setErrorStatus($purchase);
             throw new Exception('No center associated on current user');
         }
 
         if(!$quantity) {
+            $this->setErrorStatus($purchase);
             throw new Exception('Qunatity cannot be null');
+        }
+
+        //Si on est en erreur, on revalide
+        if($this->workflow->can($purchase, 'validate'))
+        {
+            $this->workflow->apply($purchase, 'validate');
+        }
+
+        if($this->workflow->can($purchase, 'revalidate'))
+        {
+            $this->workflow->apply($purchase, 'revalidate');
         }
 
         $client = new Client(['base_uri' => $this->base_uri]);
@@ -154,6 +200,5 @@ class PurchaseManager
         $this->thread($purchase, $response);
 
         return $this;
-
     }
 }
