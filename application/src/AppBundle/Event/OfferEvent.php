79c575f16463928613cda6f82ab639650ebbb90f
Loic Lao-Pane <lowkick974@gmail.com>
2017-11-28 14:11:43 +0100

    Creation de OfferEvent

---
+++ application/src/AppBundle/Event/OfferEvent.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Etudiant
+ * Date: 28/11/2017
+ * Time: 14:08
+ */
+
+namespace AppBundle\Event;
+
+
+use AppBundle\Entity\Customer;
+use AppBundle\Entity\Game;
+use Symfony\Component\EventDispatcher\Event;
+
+class OfferEvent extends Event
+{
+    /**
+     * @var Game
+     */
+    private $game;
+    /**
+     * @var Customer
+     */
+    private $customer;
+
+    public function __construct(Game $game, Customer $customer)
+    {
+        $this->game = $game;
+        $this->customer = $customer;
+    }
+
+    public function getGame()
+    {
+        return $this->game;
+    }
+
+    public function getCustomer()
+    {
+        return $this->customer;
+    }
+
+    public function getUser()
+    {
+        return $this->customer->getUser();
+    }
+}
