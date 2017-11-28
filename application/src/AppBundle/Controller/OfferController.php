3ad0af25e1aa06874c70e997900e8249d94bd178
Loic Lao-Pane <lowkick974@gmail.com>
2017-11-28 13:29:12 +0100

    Modification du controller

--- application/src/AppBundle/Controller/OfferController.php
+++ application/src/AppBundle/Controller/OfferController.php
@@ -2,48 +2,89 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Offer;
+use AppBundle\Form\Offer\OfferType;
+use Doctrine\Common\Persistence\ObjectManager;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * Class OfferController
+ * @package AppBundle\Controller
+ * @Route("/offers")
+ */
 class OfferController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/", name="offer_list")
+     * @Method({"GET"})
+     * @Security("has_role('ROLE_STAFF')")
      */
-    public function listAction()
+    public function listAction(ObjectManager $objectManager)
     {
+        $offers = $objectManager->getRepository('AppBundle:Offer')->findAll();
         return $this->render('AppBundle:Offer:list.html.twig', array(
-            // ...
+            'offers' => $offers
         ));
     }
 
     /**
-     * @Route("/create")
+     * @Route("/create", name="offer_create")
+     * @Method({"GET", "POST"})
+     * @Security("has_role('ROLE_STAFF')")
      */
-    public function createAction()
+    public function createAction(Request $request)
     {
+        $offer = new Offer();
+        $form = $this->createForm(OfferType::class, $offer);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid())
+        {
+            try
+            {
+                //save offer
+                $this->addFlash('success', 'Offer created');
+                return $this->redirectToRoute('offer_edit', array('id' => $offer->getId()));
+            }
+            catch (\Exception $exception)
+            {
+                $this->addFlash('danger', $exception->getMessage());
+            }
+
+        }
         return $this->render('AppBundle:Offer:create.html.twig', array(
-            // ...
+            'form' => $form->createView()
         ));
     }
 
     /**
-     * @Route("/{id}/edit")
+     * @Route("/{id}/edit", name="offer_edit")
+     * @Method({"GET", "POST"})
+     * @Security("has_role('ROLE_STAFF')")
      */
-    public function editAction($id)
+    public function editAction(Offer $offer, Request $request)
     {
+        $form = $this->createForm(OfferType::class, $offer);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid())
+        {
+            try {
+                //save
+                $this->addFlash('success', 'Offer '.$offer->getTitle().' update');
+            }
+            catch (\Exception $exception)
+            {
+                $this->addFlash('danger', $exception->getMessage());
+            }
+
+        }
+
         return $this->render('AppBundle:Offer:edit.html.twig', array(
-            // ...
-        ));
-    }
-
-    /**
-     * @Route("/{id}/delete")
-     */
-    public function deleteAction($id)
-    {
-        return $this->render('AppBundle:Offer:delete.html.twig', array(
-            // ...
+            'form' => $form->createView(),
+            'offer' => $offer
         ));
     }
 
