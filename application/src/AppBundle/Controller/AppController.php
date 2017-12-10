fa575228a28dea7f51c1444670afbd9f95b33c35
Loic <lowkick974@gmail.com>
2017-12-10 22:52:16 +0100

    contact form

--- application/src/AppBundle/Controller/AppController.php
+++ application/src/AppBundle/Controller/AppController.php
@@ -2,9 +2,14 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\User;
+use AppBundle\Form\ContactType;
+use AppBundle\Model\Contact;
+use Doctrine\Common\Persistence\ObjectManager;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Templating\EngineInterface;
 
 class AppController extends Controller
 {
@@ -17,5 +22,44 @@ class AppController extends Controller
         return $this->render('AppBundle:App:index.html.twig', [
 
         ]);
+    }
+
+    /**
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/contact", name="contact")
+     */
+    public function contactAction(Request $request, ObjectManager $objectManager, \Swift_Mailer $mailer, EngineInterface $templating)
+    {
+        $contact = new Contact();
+        $admins = $objectManager->getRepository(User::class)->getAdminsMails();;
+        $mails_to = [];
+        foreach($admins as $admin_mail)
+        {
+            $mails_to[] = $admin_mail['email'];
+        }
+        $form = $this->createForm(ContactType::class, $contact);
+        try {
+            $form->handleRequest($request);
+            if($form->isSubmitted() && $form->isValid())
+            {
+                $message = (new \Swift_Message('Contact'))
+                  ->setFrom($this->getParameter('mailer_sender_address'))
+                  ->setTo($mails_to)
+                    ->setBody($templating->render('AppBundle:Mail:contact.html.twig', array(
+                      'contact' => $contact
+                    )),
+                      'text/html');
+                $mailer->send($message);
+                $form = $this->createForm(ContactType::class, new Contact());
+                $this->addFlash('success', 'alert.message.sent');
+            }
+
+        } catch (\Exception $exception) {
+            $this->addFlash('danger', $exception->getMessage());
+        }
+        return $this->render('AppBundle:App:contact.html.twig', array(
+            'form' =>$form->createView()
+        ));
     }
 }
